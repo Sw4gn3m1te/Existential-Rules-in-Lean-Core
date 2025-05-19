141bf1d4aecb4b372fd306037a7dd512196a0280
Sw4gn3m1te <woingenau.lvx@web.de>
2025-05-19 16:24:14 +0200

    every_strong_core_is_weak_core

--- ExistentialRules/Models/Cores.lean
+++ ExistentialRules/Models/Cores.lean
@@ -975,5 +975,12 @@ theorem strong_core_of_universal_model_is_universal_model
         exact sc_sub.left
       . exact h_hom.right
 
-end FactSet
+  theorem every_stong_core_is_weak_core
+  (fs : FactSet sig) :
+  fs.isStrongCore → fs.isWeakCore := by
+  unfold isStrongCore isWeakCore
+  intro h1 h2 h3
+  specialize h1 h2 h3
+  exact ⟨h1.1, h1.2.1⟩
 
+end FactSet
