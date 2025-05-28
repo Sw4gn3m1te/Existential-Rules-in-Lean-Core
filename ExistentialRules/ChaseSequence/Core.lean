c078622e0aabd3964a1d48014fdc4cae00d13c1c
Sw4gn3m1te <woingenau.lvx@web.de>
2025-05-28 14:18:19 +0200

    update ws

--- ExistentialRules/ChaseSequence/Core.lean
+++ ExistentialRules/ChaseSequence/Core.lean
@@ -3,12 +3,15 @@ import ExistentialRules.Models.Basic
 import ExistentialRules.Models.Cores
 import PossiblyInfiniteTrees.PossiblyInfiniteTree.FiniteDegreeTree.Basic
 import ExistentialRules.Models.Basic
-import ExistentialRules.Triggers.RTrigger
+import ExistentialRules.Triggers.Basic
 
 variable {sig : Signature} [DecidableEq sig.P] [DecidableEq sig.C] [DecidableEq sig.V]
+variable {obs : ObsoletenessCondition sig} {kb : KnowledgeBase sig}
 
 def ChaseNode.isWeakCore {obs : ObsoletenessCondition sig} (node : ChaseNode obs rules) :
   Prop := FactSet.isWeakCore node.fact.val
 
 def ChaseNode.isStrongCore {obs : ObsoletenessCondition sig} (node : ChaseNode obs rules) :
  Prop := FactSet.isStrongCore node.fact.val
+
+theorem ChaseBranch.sequence_equals_parallel_step (cb : ChaseBranch obs kb) (ts : Set (PreTrigger sig)) : x = x := by sorry
