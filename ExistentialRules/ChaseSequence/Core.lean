1f2f18dc5df7bfb4ca789dc8729c02a4b43e2b73
Sw4gn3m1te <woingenau.lvx@web.de>
2025-08-08 02:01:18 +0200

    transferned notes

--- ExistentialRules/ChaseSequence/Core.lean
+++ ExistentialRules/ChaseSequence/Core.lean
@@ -7,6 +7,8 @@ import ExistentialRules.Triggers.Basic
 import ExistentialRules.AtomsAndFacts.Basic
 import ExistentialRules.AtomsAndFacts.SubstitutionsAndHomomorphisms
 
+--import Mathlib.Combinatorics.Graph.Basic
+
 
 variable {sig : Signature} [DecidableEq sig.P] [DecidableEq sig.C] [DecidableEq sig.V]
 variable {obs : ObsoletenessCondition sig} {kb : KnowledgeBase sig}
@@ -19,52 +21,172 @@ def ChaseNode.isStrongCore {obs : ObsoletenessCondition sig} (node : ChaseNode o
 
 ------
 
-def GroundTermMapping.applyMapSetFactSet (hs : Set (GroundTermMapping sig)) (fs : FactSet sig) : true := sorry
-
-def ChaseBranch.applyMapSetFactSetEqApplyFactSetSeq (hs : Set (GroundTermMapping sig)) (fs : FactSet sig) : true := sorry
-
-
-structure TGD extends Rule sig where
-  -- define structure of TGDs here
-  p1 : true
-  p2 : true
-
-structure EGD extends Rule sig where
- -- define structure of EGDs here
-  p1 : true
-  p2 : true
-
-def Rule.stratified (a b : Rule sig) : Prop := sorry
--- b
-infixr:50 " ≺ " => Rule.stratified
-
-def RuleSet.isWeaklyAcyclic : Prop := sorry
-
-theorem weaklyAcycIfStratified (rs : RuleSet sig) : RuleSet.isWeaklyAcyclic → ∀ r ∈ rs, r.isStratified := by sorry
-
-
--- theorem 7
-theorem ExUniversalModelIffCoreChaseHasModel : true := sorry
-
+-- define some syntactic suggar
+/-
 infixr:65 " ⊧ " => FactSet.modelsKb
 infixr:65 " ⊧ " => FactSet.modelsDb
 infixr:65 " ⊧ " => FactSet.modelsRule
 infixr:65 " ⊧ " => FactSet.modelsRules
 infixr:65 " ⊧ᵤ" => FactSet.universallyModelsKb
+-/
 
--- if A ⊧ R(x) ↔ B ⊧ R(h x)
-def GroundTermMapping.isFull (h : GroundTermMapping sig) : Prop := sorry
+def FactSet.modelsFact (fs : FactSet sig) (fact : Fact sig) : Prop := sorry
 
--- h is full injective hom.
-def GroundTermMapping.isEmbedding (h : GroundTermMapping sig) : Prop := sorry
+
+-- define CoreChaseBranch as extention from ChaseBranch
+-- Idee, ChaseBranch mit assertion, dass jede Node muss Core sein
+structure CoreChaseBranch (obs : ObsoletenessCondition sig) (kb: KnowledgeBase sig) extends ChaseBranch obs kb where
+  only_cores : sorry -- ∀ node, node ∈ branch, ChaseNode.isStrongCore node
+  -- => braucht ggf: define Membership for PossiblyInfiniteList
+
+
+namespace PossiblyInfiniteList
+
+  def toSet (l : PossiblyInfiniteList α) : Set α := sorry
+
+end PossiblyInfiniteList
+
+-- {{a,b},{a,c},{d}} -> {a,b,c,d}
+def setFlatten (S : Set (Set α)) : Set α := sorry
+
+-- theorem 7
+theorem ExUniversalModelIffCoreChaseHasModel : true := sorry
+
+  -- theorem 16, part 1 to 5
+  -- (rules : Set (TGD sig)) wie ?
+
+  -- A_0 → A_1 → A_2 → ...
+  theorem t16_1 (rules : Set (Rule sig)) (cb : ChaseBranch obs kb) : true := sorry
+
+  theorem t16_2 (A_n : PossiblyInfiniteList (FactSet sig)) (A B : FactSet sig) (rules : RuleSet sig) :
+    -- definition of membership for PossiblyInfinteList needed
+    (A = setFlatten A_n.toSet ∧ B.modelsRules rules ∧ ∃ (h : GroundTermMapping sig), h.isHomomorphism A B) → ∀ a_i, a_i ∈ A_n → ∃ (h' : GroundTermMapping sig), h'.isHomomorphism a_i B := sorry
+
+  -- t16_3
+  -- core chase result is model
+  -- core chase result is universal model
+  -- use Chasebranch.result_models_kb
+  theorem coreChaseYieldsUniversalModel (cb : CoreChaseBranch obs kb) : cb.result.universallyModelsKb kb := by sorry
+
+  theorem t16_4 (A_n : PossiblyInfiniteList (FactSet sig)) (B : FactSet sig) (rules : RuleSet sig) (h : GroundTermMapping sig) :
+    B.modelsRules rules ∧ ∀ a_i, ∃ (h : GroundTermMapping sig), a_i ∈ A_n ∧  h.isHomomorphism a_i B → ∃ (h' : GroundTermMapping sig), h.isHomomorphism sorry B := sorry
+
+  -- ∃ fs, fs = cb.result ... means the chase result is defined, probably not how we want to express it though
+  theorem t16_5 (A B : FactSet sig) (rules : RuleSet sig) (cb : ChaseBranch obs kb) :
+    (B.modelsRules rules ∧ ∃ fs, fs = cb.result ∧ ∃ (h : GroundTermMapping sig), h.isHomomorphism A B) →  ∃ (h' : GroundTermMapping sig), h'.isHomomorphism cb.result B := sorry
+
+
+-- core chase result is core
+-- strong oder weak ?
+theorem coreChaseYieldsCore (cb : CoreChaseBranch obs kb) (core : FactSet sig) : FactSet.isWeakCore cb.result := sorry
+
+
+-- finite core chase exists iff finite universal model exists
+-- two distinct core chase branches have the same result
+-- => core chase result does not depend on the order of trigger application
+
+
+-- resulting Factset of applying a set of gtm's to an existing fact set
+-- we need this when implementing a core calculation later
+def GroundTermMapping.applyMapSetFactSet (hs : Set (GroundTermMapping sig)) (fs : FactSet sig) : FactSet sig := sorry
+  -- {h.applyFactSet fs | h ∈ hs}
+
+-- parallel chase steps can be broken down intro a sequence of single-rule chase steps, both yielding the same result
+theorem ChaseBranch.applyMapSetFactSetEqApplyFactSetSeq (hs : Set (GroundTermMapping sig)) (fs : FactSet sig) : true := sorry
+
+  def FactSet.getCore (fs : FactSet sig) : FactSet sig := sorry
+
+  -- cores calculation is only neccessary after finitely many steps
+  theorem CoreCalcAfterFiniteEq : true := sorry
+
+  theorem CoreCalcIsIdempotent (fs : FactSet sig) : fs.getCore.getCore = fs.getCore  := sorry
+
+
+-- define structure of TGDs here
+structure TGD extends Rule sig where
+  -- idee hier ist eine extra liste "existential_binder" zu haben, welche alle existenziell gebundenen vars enthält
+  -- "existential_binder_is_distinct" asserted, dass nur neue vars gebunden werden können
+  -- => Was ist mit P(x,y) → ∃ x, R(x,y)  ,sagen wir einfach, dass man das nicht darf ?
+  existential_binder : List sig.V
+  existential_binder_is_distinct : ∀ v, v ∈ existential_binder → ¬ v ∈  List.map var (List.flatMap terms head)
+
+-- define structure of EGDs here
+structure EGD extends Rule sig where
+  p1 : true
+  p2 : true
+
+def Rule.prec (a b : Rule sig) : Prop := sorry
+--
+infixr:50 " ≺ " => Rule.prec
+
+-- the set of constraints in every cycle of G(Σ) is weakly acyclic (G(Σ)) is the chase graph)
+def RuleSet.isStratified (rs : RuleSet sig) : true := sorry
+
+
+structure Position (A : Atom sig) where
+  R : sig.P
+  i : Nat
+  i_in_range : 1 ≤ i ∧ i ≤ sig.arity R
+
+
+-- we should realy consider using Mathlib Graphs / SimpleGraphs
+structure Graph α where
+  V : Set α
+  E : Set (α × α)
+
+namespace Graph
+
+  def reachNext (G : Graph α) (v1 v2 : α) : Prop := (v1, v2) ∈ G.E
+
+  -- show termination, but what if we have reachability by an infinite path, do we care ?
+  def reachable (G : Graph α) (v1 v2 : α) : Prop := ∃ v, (reachNext G v1 v ∧ reachable G v v2)
+
+  def hasLoop (G : Graph α) : Prop := ∃ v, v ∈ G.V → (v, v) ∈ G.E
+
+  def hasCycle (G : Graph α) : Prop := ∃ vs, vs ⊆ G.V → true
+
+end Graph
+
+-- (rules : Set (TGD sig)) wie ?
+structure DependencyGraph (rules : Set (Rule sig)) extends Graph (Set (Rule sig)) where
+  p1 : true
+  p2 : true
+
+
+-- rs is wa if its dependency graph has no cycles with an existential edge
+def RuleSet.isWeaklyAcyclic (rs : RuleSet sig) : Prop :=
+  | DependencyGraph.
+
+
+-- All weakly-acyclic sets of TGDs and EGDs are stratified
+theorem rsWeaklyAcycIfStratified (rs : RuleSet sig) : rs.isWeaklyAcyclic → rs.isStratified := by sorry
 
 -- should we define hom indepndent of GTMs ?
+-- like make a more generell definition in the Function namespace
 
--- r : A → B ⊆ A, e is the id on dom(B)
-def GroundTermMapping.isRetract (h : GroundTermMapping sig) : Prop := sorry
+namespace GroundTermMapping
 
-def GroundTermmapping.isProperRetract (h : GroundTermMapping sig) : Prop := sorry
-  -- h.isRetract ∧ ¬ h.surjective
+  -- if A ⊧ R(x) ↔ B ⊧ R(h x)
+  def isFull (h : GroundTermMapping sig) (A B : FactSet sig) : Prop :=
+    h.isHomomorphism A B ∧ ∀ a, A.modelsFact a ↔ B.modelsFact (h.applyFact a)
+
+  -- h is full injective hom.
+  def isEmbedding (h : GroundTermMapping sig) (A B : FactSet sig) : Prop :=
+    h.isFull A B ∧ Function.injective_for_domain_set h sorry
+
+  -- r : A → B ⊆ A, e is the id on dom(B)
+  def isRetract (h : GroundTermMapping sig) (A B : FactSet sig) : Prop := sorry
+
+  def isProperRetract (h : GroundTermMapping sig) : Prop := sorry
+    -- h.isRetract ∧ ¬ h.surjective
+
+  def isExtension (h : GroundTermMapping sig) (A B C : FactSet sig) : Prop := sorry
+
+end GroundTermMapping
+
+
+
+
 
 -- if the body is machted return the head with applied h else do nothing
 -- => we maybe should split this into a Rule.apply that always applies and a Rule.isActive
@@ -73,17 +195,22 @@ def GroundTermmapping.isProperRetract (h : GroundTermMapping sig) : Prop := sorr
 -- es gibt bereits GTM.isHomomorphism
 def Rule.apply (r : Rule sig) (h : GroundTermMapping sig) (fs : FactSet sig) : FactSet sig := sorry
 
--- cores calculation is only neccessary after finitely many steps
-theorem CoreCalcAfterFiniteEq : true := sorry
 
 def FactSet.isFClosedFor (F : Set (GroundTermMapping sig)) (T : FactSet sig) (K : FactSet sig) : Prop := sorry
 
 -- needs refinement
 abbrev ModelSet := Set (FactSet sig)
-
 -- U must be finite, K cannot
 def isUniversalSetModel (U : Set (FactSet sig)) (K : Set (FactSet sig)) (F : Set (GroundTermMapping sig)) :
   ∀ M ∈ K, ∃ T ∈ U, FactSet.isFClosedFor F T M ∧
   U ⊆ K ∧
   U.finite ∧
   ¬ ∃ U' ⊂ U, isUniversalSetModel U' U F := by sorry
+
+-- chase sequence A_0,A_1... is terminating if A_n ⊧ Σ
+theorem ChaseTermIfModel (cb : ChaseBranch obs kb) (sigma : RuleSet sig) : ∃ e ∈ cb.branch, e ⊧ sigma := by sorry
+-- => Membership on possibly infinite List ?
+
+-- All chase results are hom equiv.
+theorem ChaseResultHomEq (ct : ChaseTree obs kb) (cb cb' : ChaseBranch obs kb) (cb_mem : cb ∈ ct.branches) (cb'_mem : cb' ∈ ct.branches):
+  ∃ (h : GroundTermMapping sig), h.applyFactSet cb.result = cb'.result := by sorry
